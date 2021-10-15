5f4dad822f006288e21561e6b9fce443bf8f102f
grum1n <grumin.el.pastas@gmail.com>
2021-10-15 10:01:00 +0300

    installed react-router and added router code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      testas
-    </div>
+    <>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path='/' />
+
+      </Switch>
+    </Router>
+    </>
   );
 }
 
