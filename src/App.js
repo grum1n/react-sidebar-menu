b7762ba2c396418e87f2e5e4433add87e4d1bd1d
grum1n <grumin.el.pastas@gmail.com>
2021-10-15 11:30:27 +0300

    added components to App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Reports from './pages/Reports';
+import Products from './pages/Products';
 
 function App() {
   return (
@@ -9,8 +12,9 @@ function App() {
     <Router>
       <Navbar />
       <Switch>
-        <Route path='/' />
-
+        <Route path='/' component={Home} />
+        <Route path='/reports' component={Reports} />
+        <Route path='/products' component={Products} />
       </Switch>
     </Router>
     </>
