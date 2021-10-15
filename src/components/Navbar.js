8319d3e0b257fea31580b6f5e8c7bc812854986c
grum1n <grumin.el.pastas@gmail.com>
2021-10-15 10:41:57 +0300

    added link function to Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
 import { Link } from 'react-router-dom';
+import { SidebarData } from './SidebarData';
 
 function Navbar() {
     const [sidebar, setSidebar] = useState(false);
@@ -22,6 +23,17 @@ function Navbar() {
                             <AiIcons.AiOutlineClose />
                         </Link>
                     </li>
+                    {SidebarData.map((item, index) => {
+                        return(
+                            <li key={index} className={item.cName}>
+                                <Link to={item.path}>
+                                    {item.icon}
+                                    <span>{item.title}</span>
+                                </Link>
+                            </li>
+                        )
+
+                    })}
                 </ul>
             </nav>
         </>
